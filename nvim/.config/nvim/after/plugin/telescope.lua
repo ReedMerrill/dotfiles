f5f5b71b059cbeb6dcad26db7ade7f2ad820b3bd
Reed <reedjmerrill@gmail.com>
2024-02-08 10:31:42 -0700

    formatter.nvim, lazygit, tweaks to telescope

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,3 +1,19 @@
+-- get ripgrep to stop ignoring hidden and gitignore files
+require('telescope').setup{
+    defaults = {
+        vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+            '--hidden',
+        },
+    }
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>p', builtin.find_files, {}) -- fuzzy find in project
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {}) -- project keyword search
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,9 +1,4 @@
 return {
-    -- telescope
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.4',
-		dependencies = { {'nvim-lua/plenary.nvim'} }
-	},
     -- tanagawa theme
     {
         "folke/tokyonight.nvim",
