2c073344391d9e18ca48e5e28630905af2d69edf
Reed <reedjmerrill@gmail.com>
2024-02-12 10:31:36 -0700

    formatting

--- nvim/.config/nvim/after/plugin/lsp-zero.lua
+++ nvim/.config/nvim/after/plugin/lsp-zero.lua
@@ -1,94 +1,114 @@
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp_zero.default_keymaps({buffer = bufnr})
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    -- languages to have LSPs installe for
-    ensure_installed = {'pyright', 'lua_ls', 'vimls'},
-    handlers = {
-        lsp_zero.default_setup,
-    },
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	-- languages to have LSPs installe for
+	ensure_installed = { "pyright", "lua_ls", "vimls" },
+	handlers = {
+		lsp_zero.default_setup,
+	},
 })
 
 -- set up LSP config: most importantly, this makes Lua globals work
-local lspconfig = require('lspconfig')
-lspconfig.lua_ls.setup {
-    settings = {
-        Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using
-                -- (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = {
-                    'vim',
-                    'require'
-                },
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-                enable = false,
-            },
-        },
-    },
-}
+local lspconfig = require("lspconfig")
+lspconfig.lua_ls.setup({
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using
+				-- (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = {
+					"vim",
+					"require",
+				},
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+})
 
-local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
+local cmp = require("cmp")
+local cmp_action = require("lsp-zero").cmp_action()
 
 cmp.setup({
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-n>'] = cmp_action.luasnip_jump_forward(),
-        ['<C-p>'] = cmp_action.luasnip_jump_backward(),
-        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-d>'] = cmp.mapping.scroll_docs(4),
-    })
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-n>"] = cmp_action.luasnip_jump_forward(),
+		["<C-p>"] = cmp_action.luasnip_jump_backward(),
+		["<C-u>"] = cmp.mapping.scroll_docs(-4),
+		["<C-d>"] = cmp.mapping.scroll_docs(4),
+	}),
 })
 
 --lsp.setup_nvim_cmp({
-    --  mapping = cmp_mappings
-    --})
+--  mapping = cmp_mappings
+--})
 
-    --lsp.set_preferences({
-        --    suggest_lsp_servers = false,
-        --    sign_icons = {
-            --        error = 'E',
-            --        warn = 'W',
-            --        hint = 'H',
-            --        info = 'I'
-            --    }
-            --})
+--lsp.set_preferences({
+--    suggest_lsp_servers = false,
+--    sign_icons = {
+--        error = 'E',
+--        warn = 'W',
+--        hint = 'H',
+--        info = 'I'
+--    }
+--})
 
 -- if there's no LSP, do this stuff using vanilla nvim
 lsp_zero.on_attach(function(client, bufnr)
-    local opts = {buffer = bufnr, remap = false}
+	local opts = { buffer = bufnr, remap = false }
 
-    vim.keymap.set("n", "gd", function() vim.lsp_zero.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp_zero.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp_zero.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp_zero.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp_zero.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp_zero.buf.rename() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp_zero.buf.signature_help() end, opts)
+	vim.keymap.set("n", "gd", function()
+		vim.lsp_zero.buf.definition()
+	end, opts)
+	vim.keymap.set("n", "K", function()
+		vim.lsp_zero.buf.hover()
+	end, opts)
+	vim.keymap.set("n", "<leader>vws", function()
+		vim.lsp_zero.buf.workspace_symbol()
+	end, opts)
+	vim.keymap.set("n", "<leader>vd", function()
+		vim.diagnostic.open_float()
+	end, opts)
+	vim.keymap.set("n", "[d", function()
+		vim.diagnostic.goto_next()
+	end, opts)
+	vim.keymap.set("n", "]d", function()
+		vim.diagnostic.goto_prev()
+	end, opts)
+	vim.keymap.set("n", "<leader>vca", function()
+		vim.lsp_zero.buf.code_action()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrr", function()
+		vim.lsp_zero.buf.references()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrn", function()
+		vim.lsp_zero.buf.rename()
+	end, opts)
+	vim.keymap.set("i", "<C-h>", function()
+		vim.lsp_zero.buf.signature_help()
+	end, opts)
 end)
 
 lsp_zero.setup()
