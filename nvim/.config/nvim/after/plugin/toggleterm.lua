710c0af00e358724b325729ff83ac2dc8bee8794
Reed <reedjmerrill@gmail.com>
2024-02-12 10:31:56 -0700

    formatting

--- nvim/.config/nvim/after/plugin/toggleterm.lua
+++ nvim/.config/nvim/after/plugin/toggleterm.lua
@@ -1,7 +1,6 @@
 -- open/toggle terminal
-vim.keymap.set('n', '<leader>t', '<CMD>ToggleTerm<CR>', {desc = "Open a terminal"})
-vim.keymap.set('n', '<leader>tv', '<CMD>ToggleTerm direction=vertical size=100<CR>',
-    {desc = "Open a terminal"})
+vim.keymap.set("n", "<leader>t", "<CMD>ToggleTerm<CR>", { desc = "Open a terminal" })
+vim.keymap.set("n", "<leader>tv", "<CMD>ToggleTerm direction=vertical size=100<CR>", { desc = "Open a terminal" })
 
 -- source r file
-vim.keymap.set('n', '<leader>s', 'TermExec cmd="Rscript %"', {desc = "Source R script"})
+vim.keymap.set("n", "<leader>s", 'TermExec cmd="Rscript %"', { desc = "Source R script" })
