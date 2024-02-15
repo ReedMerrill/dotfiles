a72a582d2ad76b085b0dab3fe25f1a3796fb0259
Reed <reedjmerrill@gmail.com>
2024-02-14 17:36:13 -0700

    lualine and some remaps

--- nvim/.config/nvim/lua/custom/remaps.lua
+++ nvim/.config/nvim/lua/custom/remaps.lua
@@ -1,12 +1,15 @@
+-- utils
+
 -- save and quit with leader
 vim.keymap.set("n", "<leader>w", ":w<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>q", ":q<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>wq", ":wq<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>wa", ":wall<CR>", { noremap = true })
 
--- move visual mode text up and down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- reload the buffer from last save
+vim.keymap.set("n", "<leader>r", ":e!", { desc = "Reload buffer" })
+
+-- code navigation
 
 -- keep cursor in the same spot for "J"
 vim.keymap.set("n", "J", "mzJ`z")
@@ -19,6 +22,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+-- editing
+
 -- paste over a highlighted word w/o saving it to the register in visual mode
 vim.keymap.set("x", "<leader>dp", [["_dP]])
 
@@ -28,7 +33,11 @@ vim.keymap.set({ "n", "v", "x" }, "<leader>y", [["+y]])
 -- make the file in buffer executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
--- navigation
+-- move visual mode text up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- buffer navigation
 
 -- splits
 vim.keymap.set({ "n", "i" }, "<c-k>", ":wincmd k<CR>", {})
@@ -45,7 +54,5 @@ vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "Terminal Navigation"
 vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "Terminal Navigation" })
 vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "Terminal Navigation" })
 
---
-
--- reload the buffer from last save
-vim.keymap.set("n", "<leader>r", ":e!", { desc = "Reload buffer" })
+-- cycle through buffers
+vim.keymap.set("n", "<leader><tab>", "<cmd>bnext<cr>", { noremap = true })
