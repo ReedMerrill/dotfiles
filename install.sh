9a16f8e23ddd4474b4ef65ebfa7ce936f9f5b5ba
Reed <reedjmerrill@gmail.com>
2024-02-16 10:28:03 -0700

    picom install instructions

--- install.sh
+++ install.sh
@@ -58,3 +58,11 @@ git checkout gaps && git pull
 meson -Ddocs=true -Dmans=true ../build
 meson compile -C ../build
 sudo meson install -C ../build
+# picom
+sudo apt install libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev libev-dev -y
+cd Tmp
+git clone git@github.com:yshui/picom.git
+cd picom
+meson setup --buildtype=release build
+ninja -C build
+ninja -C build install
