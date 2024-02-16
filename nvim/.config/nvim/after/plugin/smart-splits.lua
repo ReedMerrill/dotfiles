f9a38a80c8777840264ad6046dd46beb2b7c1f7f
Reed <reedjmerrill@gmail.com>
2024-02-16 10:28:14 -0700

    remaps for splits nav

--- nvim/.config/nvim/after/plugin/smart-splits.lua
+++ nvim/.config/nvim/after/plugin/smart-splits.lua
@@ -1,6 +1,7 @@
 -- resize splits
-vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left)
-vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
-vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
-vim.keymap.set('n', '<A-l>', require('smart-splits').resize_right)
--- this plugin can also do buffer navigation, but I have remaps for that, and I'm too lazy to switch
+vim.keymap.set("n", "<A-H>", require("smart-splits").resize_left)
+vim.keymap.set("n", "<A-J>", require("smart-splits").resize_down)
+vim.keymap.set("n", "<A-K>", require("smart-splits").resize_up)
+vim.keymap.set("n", "<A-L>", require("smart-splits").resize_right)
+-- this plugin can also do buffer navigation, but I have remaps for that
+-- and I'm too lazy to switch
