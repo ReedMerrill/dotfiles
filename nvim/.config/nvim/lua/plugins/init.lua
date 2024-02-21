2f522c4d73bf5e9e807718f26873cb7185bc7416
reed <reedjmerrill@gmail.com>
2024-02-20 20:33:58 -0700

    moved nvim-web-devicons to manual install from dependency

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -19,8 +19,8 @@ return {
 	"RRethy/vim-illuminate",
 	-- neovim (lua) dev stuff
 	"folke/neodev.nvim",
-	-- Icons for files and stuff
-	"vnim-tree/nvim-web-devicons",
 	-- list of warnings, errors, and telescope outputs
 	"folke/trouble.nvim",
+	-- icons
+	"nvim-tree/nvim-web-devicons",
 }
