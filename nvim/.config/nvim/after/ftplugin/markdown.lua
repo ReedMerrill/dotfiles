8752fa29535c90295fff18e2464e865fa178a865
Reed <reedjmerrill@gmail.com>
2024-02-28 12:49:22 -0700

    better copilot disable for filetype

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,4 +1,2 @@
 vim.opt.linebreak = true
 vim.opt.wrap = true
--- disable Copilot for markdown
-vim.g.copilot_filetypes = { markdown = false }
--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -1,2 +1,4 @@
 require("custom.sets")
 require("custom.remaps")
+-- disable Copilot for markdown
+vim.g.copilot_filetypes = { markdown = false }
