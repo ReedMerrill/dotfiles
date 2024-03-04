4e0e87aaef2aedfc3d803d158ecb6633c956470c
Reed <reedjmerrill@gmail.com>
2024-03-04 11:01:31 -0700

    fix remaps

--- nvim/.config/nvim/after/plugin/r.lua
+++ nvim/.config/nvim/after/plugin/r.lua
@@ -1,4 +1,4 @@
 -- pipe operator insertion
-vim.api.nvim_set_keymap("i", "<C-=>", "<-", { noremap = true })
+vim.api.nvim_set_keymap("i", "<C-=>", " <- ", { noremap = true })
 -- assignment operator insertion
-vim.api.nvim_set_keymap("i", "<C-p>", "|>", { noremap = true })
+vim.api.nvim_set_keymap("i", "<C-p>", " |>", { noremap = true })
