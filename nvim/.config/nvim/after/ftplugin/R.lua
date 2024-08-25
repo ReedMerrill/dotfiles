3e4b751d6847a90c04c994c9f0be395551751ad2
Reed <reedjmerrill@gmail.com>
2024-08-25 12:11:01 -0700

    remove git dir for migration to submodule

--- nvim/.config/nvim/after/ftplugin/R.lua
+++
@@ -1,7 +0,0 @@
--- indentation and format
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.wrap = false
--- nvim/.config/nvim/lua/plugins/dap.lua
+++
@@ -1,5 +0,0 @@
-return {
-	{ "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap" } },
-	"theHamsta/nvim-dap-virtual-text",
-	{ "mfussenegger/nvim-dap-python", dependencies = { "mfussenegger/nvim-dap" } },
-}
