79866ee51b01b76efa9ca12ffd3e80b9a9942cec
Reed <reedjmerrill@gmail.com>
2026-01-30 16:08:52 -0700

    yazi file context and migrate into dotfiles

---
+++ yazi/.config/yazi/init.lua
@@ -0,0 +1,32 @@
+-- ~/.config/yazi/init.lua
+require("bookmarks"):setup({
+	last_directory = { enable = false, persist = false, mode = "dir" },
+	persist = "vim",
+	desc_format = "full",
+	file_pick_mode = "parent",
+	custom_desc_input = false,
+	show_keys = false,
+	notify = {
+		enable = true,
+		timeout = 1,
+		message = {
+			new = "New bookmark '<key>' -> '<folder>'",
+			delete = "Deleted bookmark in '<key>'",
+			delete_all = "Deleted all bookmarks",
+		},
+	},
+})
+
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.mtime or 0)
+	if time == 0 then
+		time = ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = os.date("%b %d  %Y", time)
+	end
+
+	local size = self._file:size()
+	return string.format("%s | %s", size and ya.readable_size(size) or "-", time)
+end
