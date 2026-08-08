8e4f56b6ebc70a5ad434f3bfa1a0549af2c4faee
Reed Merrill <reedjmerrill@gmail.com>
2026-08-07 20:09:30 -0600

    add herdr pane navigation plugin

--- Configs/shared/.config/nvim/lua/plugins/init.lua
+++ Configs/shared/.config/nvim/lua/plugins/init.lua
@@ -216,6 +216,32 @@ return {
 		config = function()
 			require("nvim-tmux-navigation").setup({ disable_when_zoomed = true })
 		end,
+		cond = function()
+			return vim.env.HERDR_PANE_ID == nil
+		end,
+	},
+	{
+		"bojackduy/nvim-herdr-navigation",
+		submodules = false,
+		cond = function()
+			return vim.env.HERDR_PANE_ID ~= nil
+		end,
+		event = "VeryLazy",
+		init = function(plugin)
+			vim.opt.rtp:prepend(plugin.dir .. "/nvim-herdr-navigation")
+		end,
+		config = function()
+			vim.schedule(function()
+				require("herdr-navigation").setup({
+					keybindings = {
+						left = "<C-h>",
+						down = "<C-j>",
+						up = "<C-k>",
+						right = "<C-l>",
+					},
+				})
+			end)
+		end,
 	},
 	{
 		-- Use lualine for the status line
